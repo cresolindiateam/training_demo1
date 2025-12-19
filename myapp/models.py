ae672695a0fae59b3a18aface3e44b3d4b875a6c
Nitesh <nitesh@cresol.in>
2025-12-19 14:39:34 +0530

    14student

--- myapp/models.py
+++ myapp/models.py
@@ -41,6 +41,6 @@ class Student7(models.Model):
     name = models.CharField(max_length=50)
     age = models.IntegerField()       
     
-class Student13(models.Model):
+class Student14(models.Model):
     name = models.CharField(max_length=50)
     age = models.IntegerField()           
